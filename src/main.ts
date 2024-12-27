e1696e3d5290752526698239cd80dda4889c2e56
Carlos García <carlossalgt@gmail.com>
2024-12-27 12:44:07 -0600

    [Feature] Adding /api/v1/ path to all endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptors';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.enableCors();
 
   // Interceptors
   app.useGlobalInterceptors(new ResponseInterceptor());
